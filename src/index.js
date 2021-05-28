80e95572e7dac69dfe5c58f966a0f611436b854e
George <george.postoronca@gmail.com>
2021-05-28 14:28:43 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,57 +1,49 @@
 import "normalize.css";
 import "./styles.scss";
 
-import PageFlip from "page-flip";
+import { PageFlip } from "page-flip";
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("page loaded");
+    let demoBook = document.getElementById("demoBookExample");
+    let flipBookContent = document.querySelector(".js-flipbook-content");
 
-  const pageFlip = new PageFlip(document.getElementById("demoBookExample"), {
-    width: 550, // base page width
-    height: 733, // base page height
+    const pageFlip = new PageFlip(demoBook, {
+        width: 400, // base page width
+        height: flipBookContent.clientHeight, // base page height
 
-    size: "stretch",
-    // set threshold values:
-    minWidth: 315,
-    maxWidth: 1000,
-    minHeight: 420,
-    maxHeight: 1350,
+        size: "fixed",
+        maxHeight: 509,
+        minHeight: 320,
+        maxWidth: 800,
+        minWidth: 320,
 
-    maxShadowOpacity: 0.5, // Half shadow intensity
-    showCover: true,
-    mobileScrollSupport: false // disable content scrolling on mobile devices
-  });
+        maxShadowOpacity: 0.5, // Half shadow intensity
+        showCover: true,
+        mobileScrollSupport: false // disable content scrolling on mobile devices
+    });
 
-  console.log(pageFlip);
+    
+    // load pages
+    pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+    
+    // total pages
+    document.querySelector(".js-flipbook-total").innerText = pageFlip.getPageCount();
+    
+    document.querySelector(".js-flipbook-prev").addEventListener("click", () => {
+      pageFlip.flipPrev();
+    });
 
-  // load pages
-  pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+    document.querySelector(".js-flipbook-next").addEventListener("click", () => {
+      pageFlip.flipNext();
+    });
 
-  // document.querySelector(".page-total").innerText = pageFlip.getPageCount();
-  // document.querySelector(
-  //   ".page-orientation"
-  // ).innerText = pageFlip.getOrientation();
+    document.querySelector(".js-flipbook-fullscreen").addEventListener("click", function() {
+        // fullscreen
+        console.log("fullscreen");
+    });
 
-  // document.querySelector(".btn-prev").addEventListener("click", () => {
-  //   pageFlip.flipPrev(); // Turn to the previous page (with animation)
-  // });
-
-  // document.querySelector(".btn-next").addEventListener("click", () => {
-  //   pageFlip.flipNext(); // Turn to the next page (with animation)
-  // });
-
-  // // triggered by page turning
-  // pageFlip.on("flip", (e) => {
-  //   document.querySelector(".page-current").innerText = e.data + 1;
-  // });
-
-  // // triggered when the state of the book changes
-  // pageFlip.on("changeState", (e) => {
-  //   document.querySelector(".page-state").innerText = e.data;
-  // });
-
-  // // triggered when page orientation changes
-  // pageFlip.on("changeOrientation", (e) => {
-  //   document.querySelector(".page-orientation").innerText = e.data;
-  // });
+    // current page
+    pageFlip.on("flip", (e) => {
+      document.querySelector(".js-flipbook-current").innerText = e.data + 1;
+    });
 });
